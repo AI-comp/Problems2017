fe58a3da70445a44b21bb13160e9da13ce52bae7
Hiroki Sato <hsugar.310@gmail.com>
2017-02-14 19:04:40 +0900

    add problem 2017

---
+++ 2017/java-correct/Factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Factorial {
+	static Scanner sc;
+	
+	public int solve(){
+		int n = sc.nextInt();
+		if(n>=2017) return 0;
+		int ret = 1;
+		while(n>0){
+			ret *= n;
+			ret %= 2017;
+			n--;
+		}
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++){
+			Factorial solver = new Factorial();
+			System.out.println(solver.solve());
+		}
+
+	}
+
+}
+
---
+++ 2017/tests/validator.cpp
@@ -0,0 +1,21 @@
+#include "testlib.h"
+#include <climits>
+
+const int max_testcase = 500;
+
+void check_case(){
+	int n = inf.readInt(1, INT_MAX, "n");
+	inf.readEoln();
+}
+
+int main(){
+	registerValidation();
+	int n = inf.readInt(1, max_testcase, "T");
+	inf.readEoln();
+
+	for(int i=0; i<n ;i++){
+		check_case();
+	}
+	inf.readEof();
+	return 0;
+}
