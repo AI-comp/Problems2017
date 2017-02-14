f9c5a70468dfc991b34e01800096b4502bbc0cce
DAyama <taiki_yamada@akane.waseda.jp>
2017-02-14 16:54:12 +0900

    modified solution

--- men_g/c-correct/main.c
+++ men_g/c-correct/main.c
@@ -2,8 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
-	int a,b,c;
-	scanf("%d %d %d",&a,&b,&c);
-	printf("%d\n", a*3+b*2+c);
+	int numofcase;
+	scanf("%d",&numofcase);
+	while(numofcase--){
+		int a,b,c;
+		scanf("%d %d %d",&a,&b,&c);
+		printf("%d\n", a*3+b*2+c);
+	}
 	return 0;
 }
