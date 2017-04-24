9b2ab701521370a87b16038a50366a891d0af1b7
Hiroki Sato <hsugar.310@gmail.com>
2017-04-24 16:13:05 +0900

    testcase for a+b

--- a+b/tests/generator.py
+++ a+b/tests/generator.py
@@ -16,8 +16,9 @@ def generate_random_case(file_name, num_case, MAX_AB):
     f.close()
 
 def main():
-    generate_random_case('small.in', 10, 50)
-    generate_random_case('large.in', 50, 5000)
+#    generate_random_case('small.in', 10, 50)
+#    generate_random_case('large.in', 50, 5000)
+    generate_random_case('random.in', 50, 5000)
 
 if __name__ == '__main__':
     main()
