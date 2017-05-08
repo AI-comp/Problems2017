346c3bf1006a8dd1ecc27c930bd28e7bf7f42b37
DAyama <taiki_yamada@akane.waseda.jp>
2017-05-08 19:07:49 +0900

    add problem kataduke

---
+++ kataduke/c-correct/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LEN 1000
+
+void solve(char s[]){
+	char *res=(char *)malloc(sizeof(char)*MAX_LEN);
+	int n=strlen(s);
+	int cnt=0;
+	for(int i=0;i<n;i++){
+		if(s[i]=='.')cnt++;
+	}
+	for(int i=0;i<n;i++){
+		if(i<cnt)res[i]='.';
+		else res[i]='o';
+	}
+	printf("%s\n", res);
+}
+
+int main(){
+	int numofcase;
+	scanf("%d",&numofcase);
+	char *s = (char *)malloc(sizeof(char)*MAX_LEN);
+	while(numofcase--){
+		scanf("%s",s);
+		solve(s);
+	}
+	return 0;
+}
---
+++ kataduke/tests/generator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+//// limit
+const int smallminlen=20;
+const int smallmaxlen=50;
+const int largeminlen=100;
+const int largemaxlen=1000;
+////
+
+//// cases
+const int numofsmallcase=20;
+const int numoflargecase=50;
+string random_small_name="small.in";
+string random_large_name="large.in";
+////
+
+int RandomInt(int mini,int maxx){
+	return mini+(int)(rand()*(maxx-mini+1.0)/(1.0+RAND_MAX));
+}
+
+void generate_case(string name,int minlen,int maxlen,int numofcase){
+	FILE *fp;
+	string cs[]={"o","."};
+
+	if((fp=fopen(name.c_str(),"w"))==NULL){
+		printf("File open error!\n");
+		return ;
+	}
+	fprintf(fp, "%d\n", numofcase);
+	for(int cases=0;cases<numofcase-2;cases++){
+		string s;
+		int len=RandomInt(minlen,maxlen);
+		for(int i=0;i<len;i++){
+			s+=cs[RandomInt(0,1)];
+		}
+		fprintf(fp, "%s\n", s.c_str());
+	}
+	{
+		string s;
+		int len=maxlen;
+		for(int i=0;i<len;i++){
+			s+='.';
+		}
+		fprintf(fp, "%s\n", s.c_str());
+	}
+	{
+		string s;
+		int len=maxlen;
+		for(int i=0;i<len;i++){
+			s+='o';
+		}
+		fprintf(fp, "%s\n", s.c_str());
+	}
+	fclose(fp);
+}
+
+int main(){
+	srand((unsigned int)time(NULL));
+	generate_case(random_small_name,smallminlen,smallmaxlen,numofsmallcase);
+	generate_case(random_large_name,largeminlen,largemaxlen,numoflargecase);
+	return 0;
+}
